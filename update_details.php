105e255d6ffba604b69c73df41b582d79001dccd
Larry Browning <larry@weblabsperth.com.au>
2019-08-01 23:34:58 +0800

    Hack together the new 100 movies list using TMDB.

--- update_details.php
+++ update_details.php
@@ -19,6 +19,7 @@ $db->adult = true;  // return adult content
 
 $movieList = new MovieList();
 
+/*
 foreach ($movieList->movieDBrecs as $movieRec) {
 	$title = trim(substr($movieRec['title'],0,-6));
 	$year = substr($movieRec['title'],-5,-1);
@@ -52,6 +53,59 @@ foreach ($movieList->movieDBrecs as $movieRec) {
 	}
 
 	echo "\n\n";
+}
+*/
+
+$options = [
+	'sort_by' => 'vote_average.desc',
+	'include_video' => 'false',
+	'with_original_language' => 'en',
+	'vote_count.gte' => '7000',
+	'vote_average.gte' => '7.5',
+];
+
+$db->paged = false;
+$tmdbResult = $db->info('discover', 'movie', false, $options);
+
+if (count($tmdbResult) && $tmdbResult->total_results > 1) {
+
+	/*
+	foreach($tmdbResult->results as $movieRec) {
+		$movie = new Movie($movieRec->id);
+
+		echo "title: " . $movie->title . "\n";
+		echo "poster_path: " . $movie->poster_path . "\n";
+		echo "tmdb_id: " . $movie->id . "\n";
+		echo "imdb_id: " . $movie->imdb_id . "\n";
+		echo "release_date: " . $movie->release_date . "\n\n";
+
+		$movieList->insertMovieRec([
+			'title' => $movie->title,
+			'poster_path' => $movie->poster_path,
+			'tmdb_id' => $movie->id,
+			'imdb_id' => $movie->imdb_id,
+			'release_date' => $movie->release_date
+		]);
+	}
+	*/
+
+	$pos = 0;
+	foreach($tmdbResult->results as $movie) {
+		echo "pos no: " . ++$pos . "\n";
+		echo "title: " . $movie->title . "\n";
+		echo "poster_path: " . $movie->poster_path . "\n";
+		echo "tmdb_id: " . $movie->id . "\n";
+		echo "release_date: " . $movie->release_date . "\n\n";
+
+		$movieRec = $movieList->getMovieRec($movie->id);
+
+		$movieList->insertListLink([
+			'movie_id' => $movieRec["id"],
+			'pos_no' => $pos,
+			'list_id' => 2,
+		]);
+	}
+
 }
 
 echo "done";
