484769083b64b190b0f1a2ac6a91a8baace547cb
Larry Browning <larry@weblabsperth.com.au>
2022-07-04 23:53:40 +0800

    Increment JS/CSS versions (cache busting)

--- index.php
+++ index.php
@@ -163,7 +163,7 @@ if ($listID) {
     <meta property="fb:admins" content="810832242" />
     <meta itemprop="name" content="Scratch-off Posters | Web Labs Perth" />
     <meta itemprop="image" content="https://www.weblabsperth.com.au/100-movies/assets/img/100-movies-preview_r1a.png" />
-    <link href="./css/site.min.css?d=20220629" rel="stylesheet" />
+    <link href="./css/site.min.css?d=<?=date('Ymd')?>" rel="stylesheet" />
     <link href="assets/img/movie-play-button-icon_r1.png" rel="icon" type="image/png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
@@ -200,7 +200,7 @@ if ($listID) {
     </div>
 
     <script src="./js/soundbox.min.js"></script>
-    <script src="./js/100-movies-reveal.min.js?v=05.1"></script>
+    <script src="./js/100-movies-reveal.min.js?v=06"></script>
 </body>
 </html>
 
